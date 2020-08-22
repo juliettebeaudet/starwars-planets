136bd6885f7cf5c91232ea69add4aec7cf3b178f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-22 15:23:19 -0300

    Updates

---
+++ precache-manifest.354aca3bff3105619b8ed4242bbb57de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f954e71dc65158e08322388a1d3e22b",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "38916d684d1f70555c65",
+    "url": "/starwars-planets/static/css/main.184489bd.chunk.css"
+  },
+  {
+    "revision": "1bddbf0cdd96f5d7fcae",
+    "url": "/starwars-planets/static/js/2.32e0fb36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-planets/static/js/2.32e0fb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38916d684d1f70555c65",
+    "url": "/starwars-planets/static/js/main.201fac65.chunk.js"
+  },
+  {
+    "revision": "63af275750f9e805b0aa",
+    "url": "/starwars-planets/static/js/runtime-main.f516939e.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-planets/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "ffe919624b50d867324596a91a7f8b03",
+    "url": "/starwars-planets/static/media/star-wars-banner-art.ffe91962.jpg"
+  },
+  {
+    "revision": "6d9d6a5b5dd013ff61964151649769bd",
+    "url": "/starwars-planets/static/media/starwars-universe.6d9d6a5b.jpg"
+  }
+]);
